f6dee512e24943ea92c354aca99909899e29ece6
Alexi Carey <ciegoproductions@gmail.com>
2019-07-11 02:14:34 -0600

    Setup for port 5001, fix syntax

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 from app import create_app, socketio
 from app.db_setup import init_db
 
-app = create_app(debug=True)
+app = create_app(debug=False)
 init_db()
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, port=5001)
